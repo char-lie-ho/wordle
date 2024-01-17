79839b194a92aff73a6fad375ff1527fd2b4825d
ho921 <tho124@my.bcit.ca>
2024-01-16 22:44:29 -0800

    Check if the input guess length is 5

--- src/main/java/org/example/wordle/HelloController.java
+++ src/main/java/org/example/wordle/HelloController.java
@@ -25,19 +25,22 @@ public class HelloController {
     @FXML
     protected void checkGuess() {
         String guess = guessInput.getText();
-        Label[] row = {box00, box01, box02, box03, box04};
-        for (int i = 0; i < guess.length(); i++) {
-            String letter = guess.substring(i, i + 1);
-            row[i].setText(letter);
-            if (letter.equals(word.substring(i, i + 1))) {
-                row[i].setStyle("-fx-background-color: #5dc74e;");
-            } else if (word.contains(letter)) {
-                row[i].setStyle("-fx-background-color: #2454c9;");
-            } else {
-                row[i].setStyle("-fx-background-color: #b93737;");
+        if (guess.length() == 5) {
+            Label[] row = {box00, box01, box02, box03, box04};
+            for (int i = 0; i < guess.length(); i++) {
+                String letter = guess.substring(i, i + 1);
+                row[i].setText(letter);
+                if (letter.equals(word.substring(i, i + 1))) {
+                    row[i].setStyle("-fx-background-color: #5dc74e;");
+                } else if (word.contains(letter)) {
+                    row[i].setStyle("-fx-background-color: #2454c9;");
+                } else {
+                    row[i].setStyle("-fx-background-color: #b93737;");
+                }
             }
+            welcomeText.setText(guess);
+        } else {
+            welcomeText.setText("Please enter 5 characters.");
         }
-
-        welcomeText.setText(guess);
     }
 }
